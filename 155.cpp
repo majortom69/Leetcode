1b872322cd90a5338863d4ca44b4b3a2698f98fa
majortom69 <mtom69696969@gmail.com>
2024-09-26 21:24:14 +0300

    problem 155 solution using 2 stacks

--- 155.cpp
+++ 155.cpp
@@ -45,3 +45,54 @@ public:
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
  */
+
+
+
+
+
+ //======================[ USING 2 STACKS INSTEAD OF VECOTR ]=========================
+
+ class MinStack {
+private:
+    std::stack<int> st;
+    std::stack<int> minst;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+    st.push(val);
+    if(minst.empty() || val <= minst.top()) {
+        minst.push(val);
+    } else {
+        minst.push(minst.top());
+    }
+}
+    void pop() {
+        
+        st.pop();
+        minst.pop();
+    }
+    
+    int top() {
+        if(!st.empty()) {
+            return st.top();
+        } else return -1;
+    }
+    
+    int getMin() {
+        if(!minst.empty()) {
+            return minst.top();
+        } else return -1;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
